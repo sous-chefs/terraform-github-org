101e9e35e68edbb7eecc51434601dc1d3821a128
Dan Webb <dan.webb@damacus.io>
2017-09-20 21:52:30 +0100

    Add Nagios

--- nagios.tf
+++ nagios.tf
@@ -1,13 +1,16 @@
+module "nagios" {
+  source         = "modules/repository"
+  name           = "nagios"
+  description    = "Development repository for the Nagios cookbook"
+  homepage_url   = "https://supermarket.chef.io/cookbooks/nagios"
+  cookbook_team  = "${github_team.nagios_team.id}"
+  chef_de_partie = "${github_team.Chef_de_partie.id}"
+}
+
 resource "github_team" "nagios_team" {
   name        = "nagios"
   description = "Nagios Cookbook Maintainers"
   privacy     = "closed"
-}
-
-resource "github_team_repository" "nagios_repo" {
-  team_id    = "${github_team.nagios_team.id}"
-  repository = "nagios"
-  permission = "admin"
 }
 
 resource "github_team_membership" "nagios-maintainer-sbotman" {
