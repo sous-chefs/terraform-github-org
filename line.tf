193d5d006f2b5866dffb7500d3207bcd3de03655
Dan Webb <dan.webb@damacus.io>
2017-09-30 14:46:56 +0100

    Tidy up homepage_url and description

--- line.tf
+++ line.tf
@@ -1,7 +1,6 @@
 module "line" {
   source         = "modules/repository"
   name           = "line-cookbook"
-  description    = "Development repository for the line cookbook"
   cookbook_team  = "${github_team.line.id}"
   chef_de_partie = "${github_team.Chef_de_partie.id}"
 }
