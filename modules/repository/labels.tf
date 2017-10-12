06c005d3dda0d7760c90107df0b0f3505758f750
Dan Webb <dan.webb@damacus.io>
2017-10-12 21:42:55 +0100

    Fix repo name

--- modules/repository/labels.tf
+++ modules/repository/labels.tf
@@ -1,65 +1,65 @@
 resource "github_issue_label" "jump_in" {
-  repository = "${github_repository.repository}"
-  name       = "Type: Jump-In"
+  repository = "${github_repository.repository.name}"
+  name       = "good first issue"
   color      = "35e02e"
 }
 
 resource "github_issue_label" "feature_request" {
-  repository = "${github_repository.repository}"
+  repository = "${github_repository.repository.name}"
   name       = "Type: Feature Request"
   color      = "cbefb6"
 }
 
 resource "github_issue_label" "bug" {
-  repository = "${github_repository.repository}"
+  repository = "${github_repository.repository.name}"
   name       = "Type: Bug"
   color      = "9b1d20"
 }
 
 resource "github_issue_label" "documentation" {
-  repository = "${github_repository.repository}"
+  repository = "${github_repository.repository.name}"
   name       = "Type: Documentation"
   color      = "635d5c"
 }
 
 resource "github_issue_label" "maintenance" {
-  repository = "${github_repository.repository}"
+  repository = "${github_repository.repository.name}"
   name       = "Type: Maintenance"
   color      = "4e3e4e"
 }
 
 resource "github_issue_label" "high" {
-  repository = "${github_repository.repository}"
+  repository = "${github_repository.repository.name}"
   name       = "Priority: High"
   color      = "ad4648"
 }
 
 resource "github_issue_label" "medium" {
-  repository = "${github_repository.repository}"
+  repository = "${github_repository.repository.name}"
   name       = "Priority: Medium"
   color      = "bf6f71"
 }
 
 resource "github_issue_label" "low" {
-  repository = "${github_repository.repository}"
+  repository = "${github_repository.repository.name}"
   name       = "Priority: Low"
   color      = "d19899"
 }
 
 resource "github_issue_label" "blocked" {
-  repository = "${github_repository.repository}"
+  repository = "${github_repository.repository.name}"
   name       = "Status: Blocked"
   color      = "131611"
 }
 
 resource "github_issue_label" "in_progress" {
-  repository = "${github_repository.repository}"
+  repository = "${github_repository.repository.name}"
   name       = "Status: In Progress"
   color      = "5d6d53"
 }
 
 resource "github_issue_label" "on_hold" {
-  repository = "${github_repository.repository}"
+  repository = "${github_repository.repository.name}"
   name       = "Status: On Hold"
   color      = "675b28"
 }
