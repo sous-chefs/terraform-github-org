7f08e6646def7c154c725d1b15d6e0a5d2f0f943
Dan Webb <dan.webb@damacus.io>
2019-03-06 15:39:05 +0000

    Remove inactive members

--- modules/org_membership/main.tf
+++ modules/org_membership/main.tf
@@ -1,12 +1,3 @@
-resource "github_membership" "atomic-penguin" {
-  username = "atomic-penguin"
-  role     = "member"
-}
-
-resource "github_membership" "TD-4242" {
-  username = "TD-4242"
-  role     = "member"
-}
 
 resource "github_membership" "atrull" {
   username = "atrull"
@@ -28,41 +19,17 @@ resource "github_membership" "brianhays" {
   role     = "member"
 }
 
-resource "github_membership" "Noah_Kantrowitz" {
-  username = "coderanger"
-  role     = "member"
-}
 
 resource "github_membership" "Dylan_Myers" {
   username = "Dylan-M"
   role     = "member"
 }
 
-resource "github_membership" "Guilhem_Lettron" {
-  username = "guilhem"
-  role     = "member"
-}
-
-resource "github_membership" "Jameson" {
-  username = "jamesonjlee"
-  role     = "member"
-}
-
-resource "github_membership" "JonathanTron" {
-  username = "JonathanTron"
-  role     = "member"
-}
-
-resource "github_membership" "Joshua_Spiewak" {
-  username = "jspiewak"
-  role     = "member"
-}
 
 resource "github_membership" "Joshua_Timberman" {
   username = "jtimberman"
   role     = "member"
 }
-
 
 resource "github_membership" "Lamont_Granquist" {
   username = "lamont-granquist"
@@ -79,55 +46,23 @@ resource "github_membership" "Aaron_Kalin" {
   role     = "member"
 }
 
-resource "github_membership" "medyagh" {
-  username = "medyagh"
-  role     = "member"
-}
-
 resource "github_membership" "Brian_Menges" {
   username = "mengesb"
   role     = "member"
 }
 
-resource "github_membership" "Mike_Fiedler" {
-  username = "miketheman"
-  role     = "member"
-}
-
-resource "github_membership" "GoT" {
-  username = "PierreRambaud"
-  role     = "member"
-}
 
 resource "github_membership" "Grant_Ridder" {
   username = "shortdudey123"
   role     = "member"
 }
 
-resource "github_membership" "Sliim" {
-  username = "Sliim"
-  role     = "member"
-}
-
-resource "github_membership" "Thom_May" {
-  username = "thommay"
-  role     = "member"
-}
-
-resource "github_membership" "Xabier_de_Zuazo" {
-  username = "zuazo"
-  role     = "member"
-}
 
 resource "github_membership" "flaccid" {
   username = "flaccid"
   role     = "member"
 }
 
-resource "github_membership" "evan2645" {
-  username = "evan2645"
-  role     = "member"
-}
 
 resource "github_membership" "rshade" {
   username = "rshade"
@@ -164,35 +99,12 @@ resource "github_membership" "Elias_Abacioglu" {
   role     = "member"
 }
 
-resource "github_membership" "someara" {
-  username = "someara"
-  role     = "member"
-}
 
 resource "github_membership" "emachnic" {
   username = "emachnic"
   role     = "member"
 }
 
-resource "github_membership" "rhealitycheck" {
-  username = "rhealitycheck"
-  role     = "member"
-}
-
-resource "github_membership" "ehanlon" {
-  username = "ehanlon"
-  role     = "member"
-}
-
-resource "github_membership" "zamedic" {
-  username = "zamedic"
-  role     = "member"
-}
-
-resource "github_membership" "Marta_Paciorkowska" {
-  username = "xamebax"
-  role     = "member"
-}
 
 resource "github_membership" "snkutlu" {
   username = "snkutlu"
@@ -219,23 +131,8 @@ resource "github_membership" "atheiman" {
   role     = "member"
 }
 
-resource "github_membership" "Daniel_Paulus" {
-  username = "dpnl87"
-  role     = "member"
-}
-
 resource "github_membership" "scopenco" {
   username = "scopenco"
-  role     = "member"
-}
-
-resource "github_membership" "mateusduboli" {
-  username = "mateusduboli"
-  role     = "member"
-}
-
-resource "github_membership" "ronabop" {
-  username = "ronabop"
   role     = "member"
 }
 
@@ -259,68 +156,8 @@ resource "github_membership" "sinfomicien" {
   role     = "member"
 }
 
-resource "github_membership" "dmr" {
-  username = "gdavison"
-  role     = "member"
-}
-
-resource "github_membership" "swalberg" {
-  username = "swalberg"
-  role     = "member"
-}
-
-resource "github_membership" "pwelch" {
-  username = "pwelch"
-  role     = "member"
-}
-
-resource "github_membership" "phlipper" {
-  username = "phlipper"
-  role     = "member"
-}
-
-resource "github_membership" "jimdaga" {
-  username = "jimdaga"
-  role     = "member"
-}
-
-resource "github_membership" "sjeandeaux" {
-  username = "sjeandeaux"
-  role     = "member"
-}
-
-resource "github_membership" "miedward" {
-  username = "miedward"
-  role     = "member"
-}
-
-resource "github_membership" "scassiba" {
-  username = "sbcas"
-  role     = "member"
-}
-
-resource "github_membership" "hafrei" {
-  username = "hafrei"
-  role     = "member"
-}
-
 resource "github_membership" "majormoses" {
   username = "majormoses"
-  role     = "member"
-}
-
-resource "github_membership" "mohitsethi" {
-  username = "mohitsethi"
-  role     = "member"
-}
-
-resource "github_membership" "johnbellone" {
-  username = "johnbellone"
-  role     = "member"
-}
-
-resource "github_membership" "xorima" {
-  username = "Xorima"
   role     = "member"
 }
 
@@ -354,22 +191,7 @@ resource "github_membership" "MichaelLopez" {
   role     = "member"
 }
 
-resource "github_membership" "aeden" {
-  username = "aeden"
-  role     = "member"
-}
-
 resource "github_membership" "mthssdrbrg" {
   username = "mthssdrbrg"
-  role     = "member"
-}
-
-resource "github_membership" "adriancohea" {
-  username = "AdrianCohea"
-  role     = "member"
-}
-
-resource "github_membership" "adriennecohea" {
-  username = "AdrienneCohea"
   role     = "member"
 }
