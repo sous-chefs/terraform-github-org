4cb839510426379c7082d305a48cbb8b04a2f7f9
Aaron Kalin <akalin@martinisoftware.com>
2019-05-23 16:27:58 -0700

    I suck at renaming things

--- rvm.tf
+++ rvm.tf
@@ -1,7 +1,7 @@
 module "rvm" {
   source        = "modules/repository"
   name          = "rvm"
-  cookbook_team = "${github_team.nginx.id}"
+  cookbook_team = "${github_team.rvm.id}"
 }
 
 resource "github_team" "rvm" {
@@ -11,7 +11,7 @@ resource "github_team" "rvm" {
 }
 
 resource "github_team_membership" "rvm-maintainer-1" {
-  team_id  = "${github_team.nginx.id}"
+  team_id  = "${github_team.rvm.id}"
   username = "martinisoft"
   role     = "maintainer"
 }
