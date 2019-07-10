957af72b4a8bf30b962ac87a4e2e2e0bd114aad0
Jason Field <jason@avon-lea.co.uk>
2019-07-10 21:42:38 +0100

    Count should be a number

--- modules/repository/main.tf
+++ modules/repository/main.tf
@@ -34,7 +34,7 @@ resource "github_branch_protection" "repository_master" {
   # this is a bit of a hack to allow people to create repositories uninitialized
   # and then add branch protection later. The use cases are mostly around needing
   # to create "forked" private repositories
-  count = var.auto_init
+  count = 1
 
   repository     = "${var.name}"
   branch         = "master"
