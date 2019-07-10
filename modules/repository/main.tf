1806ab105dc7cead315c95532bc13fec60ac423d
Jason Field <jason@avon-lea.co.uk>
2019-07-10 21:31:49 +0100

    Contexts should be a string not an array now

--- modules/repository/main.tf
+++ modules/repository/main.tf
@@ -47,7 +47,7 @@ resource "github_branch_protection" "repository_master" {
 
   required_status_checks {
     strict   = "${var.require_ci_pass}"
-    contexts = ["${var.status_checks}"]
+    contexts = "${var.status_checks}"
   }
 
   required_pull_request_reviews {
