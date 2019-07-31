aa611c9b4ef13e3fa3cfb471e8f6d52b7afc08b3
Jason Field <jason@avon-lea.co.uk>
2019-07-31 18:22:30 +0100

    orbs uses lint still?

--- orbs.tf
+++ orbs.tf
@@ -2,7 +2,7 @@ module "orbs" {
   source        = "./modules/repository"
   name          = "orbs"
   cookbook_team = github_team.orbs.id
-
+  status_checks = ["ci/circleci: lint"]
   description = "The source code for orbs published by Sous-Chefs https://circleci.com/orbs/registry"
 }
 
