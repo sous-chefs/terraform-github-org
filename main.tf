de5209a91408c2ef60066b5078653bf5aacef5f5
Dan Webb <dan.webb@damacus.io>
2019-10-09 19:36:36 +0100

    Move to terraform.io backend (#210)
    * Move to terraform.io backend
    
    * Remove old config for appling in CircleCI
    
    * Remove validate from CirlceCI

--- main.tf
+++ main.tf
@@ -4,13 +4,17 @@ module "membership" {
 
 provider "github" {
   token        = var.github_token
-  organization = var.github_organization
+  organization = "sous-chefs"
 }
 
+variable "github_token" {}
+
 terraform {
-  backend "s3" {
-    bucket = "sous-chefs.terraform-state"
-    key    = "terraform.tfstate"
-    region = "eu-west-1"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "sous-chefs"
+    workspaces {
+      name = "terraform-github-org"
+    }
   }
 }
