7e75a139e7c34089d9405b8ed0a3917b90cd718f
Mark E. Schill <Mark.Schill@cmschill.net>
2019-10-21 08:13:46 -0400

    Removes status_checks from Varnish cookbook (#223)

--- varnish.tf
+++ varnish.tf
@@ -2,7 +2,6 @@ module "varnish" {
   source        = "./modules/repository"
   name          = "varnish"
   cookbook_team = github_team.varnish.id
-  status_checks = ["ci/circleci: lint"]
 }
 
 resource "github_team" "varnish" {
