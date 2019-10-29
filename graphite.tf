51e70c99f8a05caa49bef150417c2ac236894603
John Roesler <johnrroesler@gmail.com>
2019-10-29 18:54:29 -0500

    Remove lint requirement graphite

--- graphite.tf
+++ graphite.tf
@@ -2,7 +2,6 @@ module "graphite" {
   source        = "./modules/repository"
   name          = "graphite"
   cookbook_team = github_team.graphite.id
-  status_checks = ["ci/circleci: lint"]
 }
 
 resource "github_team" "graphite" {
