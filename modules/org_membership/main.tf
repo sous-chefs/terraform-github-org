b63b99ac51d98bd8d3628f5f1d12626fc494f036
Darwin <5746693+darwin67@users.noreply.github.com>
2019-11-05 10:22:15 -0800

    PR feedback

--- modules/org_membership/main.tf
+++ modules/org_membership/main.tf
@@ -218,7 +218,7 @@ resource "github_membership" "JanStevens" {
   role     = "member"
 }
 
-resource "github_membership" "DarwinWu" {
+resource "github_membership" "darwin67" {
   username = "darwin67"
   role     = "member"
 }
