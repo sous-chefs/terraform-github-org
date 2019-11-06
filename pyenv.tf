c8e75f14c066ff9860617c3f7fdd63805eec9be7
Jason Field <jason@avon-lea.co.uk>
2019-11-06 21:24:46 +0000

    pyenv fixes

--- pyenv.tf
+++ pyenv.tf
@@ -1,7 +1,7 @@
 module "pyenv" {
   source        = "./modules/repository"
   name          = "pyenv"
-  cookbook_team = github_team.chef-pyenv.id
+  cookbook_team = github_team.pyenv.id
 }
 
 resource "github_team" "pyenv" {
@@ -11,13 +11,13 @@ resource "github_team" "pyenv" {
 }
 
 resource "github_team_membership" "pyenv-maintainer-1" {
-  team_id  = github_team.chef-pyenv.id
+  team_id  = github_team.pyenv.id
   username = "darwin67"
   role     = "maintainer"
 }
 
 resource "github_team_membership" "pyenv-maintainer-2" {
-  team_id  = github_team.chef-pyenv.id
+  team_id  = github_team.pyenv.id
   username = "Xorima"
   role     = "maintainer"
 }
