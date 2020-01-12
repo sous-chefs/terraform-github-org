ccbea18cfc1a7c6cd0f54a7e531716e1d89eea3a
Xorima <4923914+Xorima@users.noreply.github.com>
2020-01-12 19:24:05 +0000

    Use actions by default everywhere (#234)

--- apache2.tf
+++ apache2.tf
@@ -2,7 +2,6 @@ module "apache2" {
   source        = "./modules/repository"
   name          = "apache2"
   cookbook_team = github_team.apache2.id
-  status_checks = ["final", "danger/danger"]
 }
 
 resource "github_team" "apache2" {
