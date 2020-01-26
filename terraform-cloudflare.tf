68b243e2435e7641ee8e67926833706c2feedb0b
Jason Field <jason@avon-lea.co.uk>
2020-01-26 10:29:20 +0000

    Add statuc checks
    Signed-off-by: Jason Field <jason@avon-lea.co.uk>

--- terraform-cloudflare.tf
+++ terraform-cloudflare.tf
@@ -4,4 +4,5 @@ module "terraform-cloudflare" {
   cookbook_team = github_team.board.id
   description   = "Repository to manage our Cloudflare settings"
   homepage_url  = "https://cloudflare.com"
+  status_checks = ["atlas/sous-chefs/terraform-cloudflare", "terraform-lint"]
 }
