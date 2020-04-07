f41fbcf9acf93da69f01993b12ff09878f7d660c
John Roesler <johnrroesler@gmail.com>
2020-04-06 22:41:28 -0500

    emachnic was duplicated

--- modules/org_membership/main.tf
+++ modules/org_membership/main.tf
@@ -232,8 +232,3 @@ resource "github_membership" "ramereth" {
   username = "ramereth"
   role     = "member"
 }
-
-resource "github_membership" "emachnic" {
-  username = "emachnic"
-  role     = "member"
-}
