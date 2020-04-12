7a84cca5ac6361f463efd193da8ef138648dbfc9
Jason Field <jason@avon-lea.co.uk>
2020-04-12 10:30:33 +0100

    Add terraform-managed topic to all repositories that terraform manages

--- modules/repository/variables.tf
+++ modules/repository/variables.tf
@@ -66,7 +66,7 @@ variable "archived" {
 }
 
 locals {
-  default_topics = ["chef", "chef-cookbook", "chef-resource", "${replace(var.name, "_", "-")}"]
+  default_topics = ["chef", "chef-cookbook", "chef-resource", "${replace(var.name, "_", "-")}", "terraform-managed"]
   topics         = concat(local.default_topics, var.additional_topics)
 }
 
