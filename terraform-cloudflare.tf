15be78d042f1e2f99c0abf2e772545841e5e7f90
Jason Field <jason@avon-lea.co.uk>
2020-06-02 12:12:47 +0100

    Fix cloudflare checks

--- terraform-cloudflare.tf
+++ terraform-cloudflare.tf
@@ -4,6 +4,6 @@ module "terraform-cloudflare" {
   cookbook_team     = github_team.board.id
   description       = "Repository to manage our Cloudflare settings"
   homepage_url      = "https://cloudflare.com"
-  status_checks     = ["atlas/sous-chefs/terraform-cloudflare", "terraform-lint"]
+  status_checks     = ["Terraform Cloud/sous-chefs/terraform-cloudflare", "terraform-lint"]
   additional_topics = ["terraform", "dns", "cloudflare"]
 }
