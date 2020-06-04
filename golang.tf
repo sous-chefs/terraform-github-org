a7db1dc7cac552f4091a72916d7695aee3e37b84
Jason Field <jason@avon-lea.co.uk>
2020-06-04 18:30:20 +0100

    Add Prometheus and golang

---
+++ golang.tf
@@ -0,0 +1,23 @@
+module "golang" {
+  source        = "./modules/repository"
+  name          = "golang"
+  cookbook_team = github_team.golang.id
+}
+
+resource "github_team" "golang" {
+  name        = "golang"
+  description = "golang Cookbook Maintainers"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "golang-maintainer-1" {
+  team_id  = github_team.golang.id
+  username = "jeffbyrnes"
+  role     = "member"
+}
+
+resource "github_team_membership" "golang-maintainer-2" {
+  team_id  = github_team.golang.id
+  username = "JohnRoesler"
+  role     = "member"
+}
---
+++ prometheus.tf
@@ -0,0 +1,23 @@
+module "prometheus" {
+  source        = "./modules/repository"
+  name          = "prometheus"
+  cookbook_team = github_team.prometheus.id
+}
+
+resource "github_team" "prometheus" {
+  name        = "prometheus"
+  description = "prometheus Cookbook Maintainers"
+  privacy     = "closed"
+}
+
+resource "github_team_membership" "prometheus-maintainer-1" {
+  team_id  = github_team.prometheus.id
+  username = "mbaitelman"
+  role     = "member"
+}
+
+resource "github_team_membership" "prometheus-maintainer-2" {
+  team_id  = github_team.prometheus.id
+  username = "ramereth"
+  role     = "member"
+}
