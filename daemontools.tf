6b05dacb0d99939d2dfcd8ab19bbb02d8df4a4ba
Tomoya Kabe <limit.usus@gmail.com>
2020-09-16 14:22:58 +0900

    Comment repository out

--- daemontools.tf
+++ daemontools.tf
@@ -1,17 +1,17 @@
-module "daemontools" {
-  source        = "./modules/repository"
-  name          = "daemontools"
-  cookbook_team = github_team.daemontools.id
-}
-
-resource "github_team" "daemontools" {
-  name        = "daemontools"
-  description = "daemontools Cookbook Maintainers"
-  privacy     = "closed"
-}
-
-resource "github_team_membership" "daemontools-maintainer-1" {
-  team_id  = github_team.daemontools.id
-  username = "limitusus"
-  role     = "maintainer"
-}
+#module "daemontools" {
+#  source        = "./modules/repository"
+#  name          = "daemontools"
+#  cookbook_team = github_team.daemontools.id
+#}
+#
+#resource "github_team" "daemontools" {
+#  name        = "daemontools"
+#  description = "daemontools Cookbook Maintainers"
+#  privacy     = "closed"
+#}
+#
+#resource "github_team_membership" "daemontools-maintainer-1" {
+#  team_id  = github_team.daemontools.id
+#  username = "limitusus"
+#  role     = "maintainer"
+#}
