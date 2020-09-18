9b1ef00ced609c73815381c2b92486ea518e8d4a
Jason Field <jason@avon-lea.co.uk>
2020-09-18 16:07:12 +0100

    Fix chef-appveyor-ci blowing up tf

--- chef-appveyor-ci.tf
+++ chef-appveyor-ci.tf
@@ -1,17 +1,17 @@
-module "chef-appveyor-ci" {
-  source        = "./modules/repository"
-  name          = "chef-appveyor-ci"
-  cookbook_team = github_team.chef-appveyor-ci.id
-}
+# module "chef-appveyor-ci" {
+#   source        = "./modules/repository"
+#   name          = "chef-appveyor-ci"
+#   cookbook_team = github_team.chef-appveyor-ci.id
+# }
 
-resource "github_team" "chef-appveyor-ci" {
-  name        = "chef-appveyor-ci"
-  description = "chef-appveyor-ci Cookbook Maintainers"
-  privacy     = "closed"
-}
+# resource "github_team" "chef-appveyor-ci" {
+#   name        = "chef-appveyor-ci"
+#   description = "chef-appveyor-ci Cookbook Maintainers"
+#   privacy     = "closed"
+# }
 
-resource "github_team_membership" "chef-appveyor-ci-maintainer-1" {
-  team_id  = github_team.chef-appveyor-ci.id
-  username = "tas50"
-  role     = "maintainer"
-}
+# resource "github_team_membership" "chef-appveyor-ci-maintainer-1" {
+#   team_id  = github_team.chef-appveyor-ci.id
+#   username = "tas50"
+#   role     = "maintainer"
+# }
