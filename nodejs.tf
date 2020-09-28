22f2404130e27a071da73016ca57fd267ae0aa13
MarkGibbons <gibbonsmj80@gmail.com>
2020-09-28 10:27:52 -0700

    Request maintainer access to the nodejs cookbook

--- nodejs.tf
+++ nodejs.tf
@@ -15,3 +15,9 @@ resource "github_team_membership" "nodejs-maintainer-1" {
   username = "tas50"
   role     = "maintainer"
 }
+
+resource "github_team_membership" "nodejs-maintainer-2" {
+  team_id  = github_team.nodejs.id
+  username = "MarkGibbons"
+  role     = "maintainer"
+}
